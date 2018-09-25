0ddd6096dffc9fcc66eec2b988a851e6da8558e7
tianxuning <xtian513@163.com>
2018-09-25 20:05:35 +0800

    inital commit

---
+++ build/webpack.config.dev.js
@@ -0,0 +1,45 @@
+'use strict'
+
+const webpack = require('webpack')
+const { VueLoaderPlugin } = require('vue-loader')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+const HOST = 'localhost'
+const PORT = 8080
+
+module.exports = {
+  mode: 'development',
+  
+  entry: [
+    './src/app.js'
+  ],
+
+  devServer: {
+    hot: true,
+    open: true,
+    host: HOST,
+    port: PORT,
+    watchOptions: {
+      poll: true
+    }
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        use: 'vue-loader'
+      }
+    ]
+  },
+
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new VueLoaderPlugin(),
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: 'index.html',
+      inject: true
+    })
+  ]
+}
---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <h1 id="test">Hello wwwdddwww</h1>
+  </div>
+</template>
+
+<style scoped>
+.test {
+	color: blue;
+}
+</style>
