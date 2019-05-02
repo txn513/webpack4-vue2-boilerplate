0f9ebf26349afc5c813ee6c8b5bf931c897b9508
Lance Tian <xtian513@gmail.com>
2019-05-02 21:44:23 +0800

    add proxy

--- build/webpack.config.dev.js
+++ build/webpack.config.dev.js
@@ -31,7 +31,16 @@ module.exports = merge(baseConfig, {
     quiet: true,
     watchOptions: {
       poll: true
-    }
+    },
+    // proxy: {
+    //   '/api': {
+    //       target: 'http://localhost:9000',//后端接口地址
+    //       changeOrigin: true,//是否允许跨越
+    //       pathRewrite: {
+    //           '^/api': '',//重写,
+    //       }
+    //   }
+    // }
   },
 
   plugins: [
